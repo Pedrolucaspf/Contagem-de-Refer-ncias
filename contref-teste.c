618fc5907da3a4b709b2eb05a7330fb578306390
Pedrolucaspf <pedrolucas.portelafreitas@gmail.com>
2024-08-07 14:13:00 -0300

    Update contref-teste.c

--- contref-teste.c
+++ contref-teste.c
@@ -9,28 +9,24 @@ int main(){
   *w = 20;
   printf("v = %d, w = %d\n", *v, *w);
   mostraCont(v);
-  atrib2(v, w);
+  atrib2(&v, w);
   mostraCont(v);
   printf("\nv = %d, w = %d\n", *v, *w);
-  atrib2(v, NULL);
+  atrib2(&v, NULL);
   mostraCont(w);
-  if(v==NULL){
-    printf("\nv = null.\n");
-  }
+  mostraCont(v);
   printf("\n");
 
   *c = 'a';
   *d = 'b';
   printf("c = %c, d = %c\n", *c, *d);
   mostraCont(c);
-  atrib2(c, d);
+  atrib2(&c, d);
   mostraCont(c);
   printf("\nc = %c, d = %c\n", *c, *d);
-  atrib2(c, NULL);
+  atrib2(&c, NULL);
   mostraCont(d);
-  if(c==NULL){
-    printf("\nc = null.");
-  }
+  mostraCont(c);
   free_ponteiros();
   return 0;
 } 
